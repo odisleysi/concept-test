4a38afaa6ebda80a8c112a2ad75c851ae253fe8c
Odisleysi Furones <omartinez@factorclick.cl>
2020-10-21 23:30:27 -0400

    #2 pc sobre recordRTC testing esa lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import * as RecordRTC from 'recordrtc';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'concept-test';
+
+  @ViewChild('video')
+  video: any;
+
+  recordRTC: any;
+  stream: any;
+
+  ngAfterViewInit() {
+    // set the initial state of the video
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = false;
+    video.controls = true;
+    video.autoplay = false;
+  }
+
+  startRecording() {
+    let mediaConstraints: MediaStreamConstraints = {
+      video: {
+        width: 160,
+        height: 120,
+        // mandatory: {
+        //   minWidth: 1280,
+        //   minHeight: 720
+        // }
+      }, audio: true
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  errorCallback(error: any) {
+    console.log({error});
+  }
+
+  successCallback(stream: MediaStream) {
+    var options = {
+      mimeType: 'video/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
+      audioBitsPerSecond: 128000,
+      videoBitsPerSecond: 128000,
+      bitsPerSecond: 128000 // if this line is provided, skip above two
+    };
+    this.stream = stream;
+    this.recordRTC = RecordRTC(stream, options);
+    this.recordRTC.startRecording();
+    let video: HTMLVideoElement = this.video.nativeElement;
+    video.src = window.URL.createObjectURL(stream);
+    this.toggleControls();
+  }
+
+  toggleControls() {
+    const video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = !video.muted;
+    video.controls = !video.controls;
+    video.autoplay = !video.autoplay;
+  }
+
+  stopRecording() {
+    const recordRTC = this.recordRTC;
+    recordRTC.stopRecording(this.processVideo.bind(this));
+    const stream = this.stream;
+    stream.getAudioTracks().forEach(track => track.stop());
+    stream.getVideoTracks().forEach(track => track.stop());
+  }
+
+  processVideo(audioVideoWebMURL) {
+    let video: HTMLVideoElement = this.video.nativeElement;
+    let recordRTC = this.recordRTC;
+    video.src = audioVideoWebMURL;
+    this.toggleControls();
+    var recordedBlob = recordRTC.getBlob();
+    recordRTC.getDataURL(function (dataURL) { });
+  }
+
+  download() {
+    this.recordRTC.save('video.webm');
+  }
 }
